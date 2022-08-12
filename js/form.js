f5b78c2bd29c82686f44b56330bea4f2a6ae257b
fmiguezo <miguezflorencia@gmail.com>
2022-08-12 11:09:10 -0300

    :sparkles: Adding Fetch version in users and printing form info in DOM

--- js/form.js
+++ js/form.js
@@ -3,10 +3,24 @@ function obtenerDatos() {
   const email = document.getElementById("email").value;
   const mensaje = document.getElementById("mensaje").value;
 
+  // Imprimir en consola
   document.querySelector("#formulario").addEventListener("submit", function (e) {
     e.preventDefault();
     console.log(`Nombre: ${nombre} 
 Email: ${email} 
 Mensaje: ${mensaje}`);
+
+    // Imprimir en DOM
+    let body = ``;
+    body += `
+<div>
+    <h3>Resumen de datos enviados</h3>
+    <p>Nombre: ${nombre}</p>
+    <p>Email: ${email}</p>
+    <p>Mensaje: ${mensaje}</p>
+    
+</div>
+`;
+    document.getElementById("print-results").innerHTML = body;
   });
 }
--- js/users.js
+++ js/users.js
@@ -1,8 +1,11 @@
 const URL_USERS = "https://jsonplaceholder.typicode.com/users";
 
-const users = axios.get(URL_USERS);
-const responseAPIAxios = users.then((response) => {
-  const data = response.data;
+fetch(URL_USERS)
+  .then((res) => res.json())
+  .then((data) => showData(data))
+  .catch((err) => console.log("Error: ", err));
+
+const showData = (data) => {
   let body = ``;
   data.map((user) => {
     const { id, name, email, phone, address, company } = user;
@@ -18,4 +21,26 @@ const responseAPIAxios = users.then((response) => {
       `;
     document.getElementById("user-info").innerHTML = body;
   });
-});
+};
+
+// ----------- Version with Axios ---------------- //
+
+// const users = axios.get(URL_USERS);
+// const responseAPIAxios = users.then((response) => {
+//   const data = response.data;
+//   let body = ``;
+//   data.map((user) => {
+//     const { id, name, email, phone, address, company } = user;
+//     body += `
+//         <tr>
+//             <td>${id}</td>
+//             <td>${name}</td>
+//             <td>${email}</td>
+//             <td>${address.city}</td>
+//             <td>${phone}</td>
+//             <td>${company.name}</td>
+//         </tr>
+//       `;
+//     document.getElementById("user-info").innerHTML = body;
+//   });
+// });
